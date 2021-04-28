eb2dc69b4174234a9f461ed075cd47a4594389b3
Marco Carletti <mcarlett@redhat.com>
2021-04-28 16:53:08 +0200

    Add user config

--- release.sh
+++ release.sh
@@ -15,6 +15,9 @@ echo "Pull"
 git pull origin $DEVEL_BRANCH
 echo "Prepare release with version $RELEASE_VERSION and set development version $DEVEL_VERSION"
 mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
+echo "Set user config"
+git config user.email $GITHUB_ACTOR@workflow.github.com
+git config user.name $GITHUB_ACTOR
 echo "Perform release"
 mvn release:perform
 echo "Rebase branch $RELEASE_BRANCH"
