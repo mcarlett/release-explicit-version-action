f6b3cb878acde0b3315c92c9f9917f4c15cbcc55
Marco Carletti <mcarlett@redhat.com>
2021-04-28 22:05:20 +0200

    Create tag from release plugin

--- release.sh
+++ release.sh
@@ -15,14 +15,14 @@ git checkout $DEVEL_BRANCH
 echo "Pull"
 git pull origin $DEVEL_BRANCH
 echo "Prepare release with version $RELEASE_VERSION and set development version $DEVEL_VERSION"
-mvn -B -DskipTests -Darguments="-DskipTests -DremoteTagging=false" release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
+mvn -B -DskipTests -Darguments="-DskipTests" -Dtag=$TAG release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
 echo "Perform release"
 mvn -DskipTests -Darguments="-DskipTests -Dmaven.deploy.skip=true" release:perform
-echo "Checkout branch $RELEASE_BRANCH"
-git checkout $RELEASE_BRANCH
-git pull origin $RELEASE_BRANCH
-echo "Create tag $TAG on $RELEASE_BRANCH"
-git tag -a -m "Release tag" $TAG
-git push --follow-tags
+#echo "Checkout branch $RELEASE_BRANCH"
+#git checkout $RELEASE_BRANCH
+#git pull origin $RELEASE_BRANCH
+#echo "Create tag $TAG on $RELEASE_BRANCH"
+#git tag -a -m "Release tag" $TAG
+#git push --follow-tags
 echo "::set-output name=generated-tag::$(echo $TAG)"
 echo "Done!"
