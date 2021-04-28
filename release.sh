b6967896494847c2727c72e42e3c5002ac7a6662
Marco Carletti <mcarlett@redhat.com>
2021-04-28 17:08:30 +0200

    Fix error

--- release.sh
+++ release.sh
@@ -5,8 +5,9 @@ set -e
 
 TAG=v$RELEASE_VERSION
 
-env
-
+echo "Set user config"
+git config --global user.email "$GITHUB_ACTOR@workflow.github.com"
+git config --global user.name "$GITHUB_ACTOR"
 echo "Fetch remote"
 git fetch
 echo "Checkout $DEVEL_BRANCH"
@@ -14,10 +15,7 @@ git checkout $DEVEL_BRANCH
 echo "Pull"
 git pull origin $DEVEL_BRANCH
 echo "Prepare release with version $RELEASE_VERSION and set development version $DEVEL_VERSION"
-mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
-echo "Set user config"
-git config --global user.email "$GITHUB_ACTOR@workflow.github.com"
-git config --global user.name "$GITHUB_ACTOR"
+mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION -DskipTests
 echo "Perform release"
 mvn release:perform
 echo "Rebase branch $RELEASE_BRANCH"
