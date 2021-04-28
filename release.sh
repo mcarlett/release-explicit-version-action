09d5a35b4076dbe0d916802e34664c38e825f5c2
Marco Carletti <mcarlett@redhat.com>
2021-04-28 15:49:59 +0200

    Add fetc upstream

--- release.sh
+++ release.sh
@@ -11,19 +11,17 @@ echo "Fetch remote"
 git fetch
 echo "Checkout $DEVEL_BRANCH"
 git checkout $DEVEL_BRANCH
-echo "Fetch remote"
-git fetch upstream
-echo "Pull upstream"
-git pull upstream $DEVEL_BRANCH
+echo "Pull"
+git pull
 echo "Prepare release with version $RELEASE_VERSION and set development version $DEVEL_VERSION"
 mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
 echo "Perform release"
 mvn release:perform
 echo "Rebase branch $RELEASE_BRANCH"
 git checkout $RELEASE_BRANCH
-git pull upstream $RELEASE_BRANCH
+git pull
 git rebase $TAG
 echo "Create release tag $TAG"
-git push upstream $DEVEL_BRANCH $RELEASE_BRANCH $TAG
+git push $DEVEL_BRANCH $RELEASE_BRANCH $TAG
 echo "::set-output name=generated-tag::$(echo $TAG)"
 echo "Done!"
