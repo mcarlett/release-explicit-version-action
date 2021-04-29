7fdeda2bb4a0c4ee55f4720fa0f3e525ee2e2e00
Marco Carletti <mcarlett@redhat.com>
2021-04-29 11:26:01 +0200

    Change checkout flow

--- release.sh
+++ release.sh
@@ -10,20 +10,23 @@ git config --global user.email "$GITHUB_ACTOR@workflow.github.com"
 git config --global user.name "$GITHUB_ACTOR"
 echo "Fetch remote"
 git fetch
+echo "Checkout $RELEASE_BRANCH"
+git checkout $RELEASE_BRANCH
+echo "Pull"
+git pull origin $DEVEL_BRANCH
 echo "Checkout $DEVEL_BRANCH"
 git checkout $DEVEL_BRANCH
 echo "Pull"
 git pull origin $DEVEL_BRANCH
+git pull origin $RELEASE_BRANCH
 echo "Prepare release with version $RELEASE_VERSION and set development version $DEVEL_VERSION"
 mvn -B -DskipTests -Darguments="-DskipTests" -DpreparationGoals="clean package" -DpushChanges=false -DsignTag=true -Dtag=$TAG release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVEL_VERSION
 echo "Perform release"
 mvn -DskipTests -Darguments="-DskipTests -Dmaven.deploy.skip=true" release:perform
 echo "Checkout branch $RELEASE_BRANCH"
-git fetch
 git checkout $RELEASE_BRANCH
-git pull origin $RELEASE_BRANCH
 echo "Rebase $TAG into $RELEASE_BRANCH"
 git rebase $TAG
-git push origin $RELEASE_BRANCH
+git push origin $RELEASE_BRANCH $DEVEL_BRANCH --follow-tags
 echo "::set-output name=generated-tag::$(echo $TAG)"
 echo "Done!"
